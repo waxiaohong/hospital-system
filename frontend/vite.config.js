75070b72625dff156632ba4f16482d6312141e6d
buerka <buerka@buerka.cn>
2026-02-12 19:26:50 +0800

    refactor: 适应开发环境调整

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import yaml from 'js-yaml'
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const configPath = path.resolve(__dirname, './config.yaml')
 let backendPort = 8080 
-
-try {
-  const fileContents = fs.readFileSync(configPath, 'utf8')
-  const config = yaml.load(fileContents)
-  if (config?.server?.port) {
-    backendPort = config.server.port
-    console.log(`[Vite] 成功从 config.yaml 读取后端端口: ${backendPort}`)
-  }
-} catch (e) {
-  console.error('[Vite] 未读取到 config.yaml 使用默认端口 8080', e)
-}
 
 export default defineConfig({
   plugins: [react()],
